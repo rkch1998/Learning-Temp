f1a4d769388e8069b4ec4841c3dd9a148cf3f8be
Ravi Chauhan <85224620+rkch1998@users.noreply.github.com>
2024-05-17 18:26:47 +0530

    Initial Commit

--- Learning/DB/src/main/java/com/connect/DB/DbApplication.java
+++ Learning/DB/src/main/java/com/connect/DB/DbApplication.java
@@ -1,13 +1,33 @@
 package com.connect.DB;
 
+import com.connect.DB.service.DataBaseConnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class DbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DbApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+
+		try {
+			Connection connection = DataBaseConnect.getConnection();
+
+			PreparedStatement statement = connection.prepareStatement("SELECT 1");
+			ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt(1));
+			}
+			resultSet.close();
+			statement.close();
+			DataBaseConnect.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ Learning/DB/src/main/java/com/connect/DB/service/DataBaseConnect.java
@@ -0,0 +1,36 @@
+package com.connect.DB.service;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataBaseConnect {
+    private static HikariDataSource dataSource;
+
+    static {
+        initializeDataSource();
+    }
+
+    private static void initializeDataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://103.158.108.17:5432/1_CygnetGSPTenant_1");
+        config.setUsername("CygGSPDBA");
+        config.setPassword("Admin#321");
+
+        config.setMaximumPoolSize(10);
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void close(){
+        if(dataSource != null){
+            dataSource.close();
+        }
+    }
+}
